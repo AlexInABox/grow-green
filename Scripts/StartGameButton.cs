7b5a7f41b4f3db66baea12bfbed6f38367d540c8
AlexInABox <main@alexinabox.de>
2024-08-27 20:39:02 +0200

    a button!?

--- Scripts/StartGameButton.cs
+++ Scripts/StartGameButton.cs
@@ -1,15 +1,29 @@
 using Godot;
 using System;
 
-public partial class TitleScreenButtonManager : Button
+public partial class StartGameButton : Button
 {
+
+	bool userPressed = false;
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		Pressed += ButtonPressed;
+	}
+
+	private void ButtonPressed()
+	{
+		GD.Print("Hello world!");
+		Text = "WARUM??!?!";
+		userPressed = true;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (userPressed) {
+			Text += "!?";
+		}
 	}
 }
