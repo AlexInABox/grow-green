0c577f3ea042a34fefee6099337a4449d80df8a4
joshnicot06 <j.tischer@fu-berlin.de>
2024-09-11 11:37:36 +0200

    Clickable Plant Setup

---
+++ Scripts/ClickablePlant.cs
@@ -0,0 +1,48 @@
+using Godot;
+using System;
+using GrowGreen.Scripts;
+
+public partial class ClickablePlant : Button
+{
+
+	int counter = 0;
+	Plant plant = new Plant();
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Pressed += ButtonPressed;
+		plant.waterStatus = 50;
+		plant.wateringImpact = 8;
+		plant.decayRate = 2;
+		Text = plant.GetWaterStatus().ToString();
+
+	}
+
+	private void ButtonPressed()
+	{
+		if (plant.CheckForDead() == false)
+		{
+			plant.WaterPlant();
+			Text = plant.GetWaterStatus().ToString();
+			bool killPlant = plant.CheckForDead();
+			if (killPlant == true)
+			{
+				Text = "x";
+			}
+
+			
+		}
+		else
+		{
+			QueueFree();
+			return;
+		}
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	
+	}
+}
--- Scripts/Plant.cs
+++ Scripts/Plant.cs
@@ -4,21 +4,37 @@ using System;
 public partial class Plant : Node
 {
 
-	int cost;
-	int sellValue;
-	int yield;
-	float growDurationInDays;
-	float currentGrowProgress;
-	int stage;
-	String name;
-	String description;
-	float cycle;
-	float waterStatus;
-	bool isDead;
-	float wateringImpact;
+	public int cost; //Cost to purchase
+	public int sellValue; //Value when fully grown
+	public int yield; //Money Production per Day when fully grown
+	public int growDurationInDays; //Needed days for next stage (e.g. 7 days)
+	public int currentGrowProgress; //Current Progress (e.g. 3/7 days)
+	public int stage; //Current Stage 0=DriedOut, 1=Seed .... 3=FullyGrown, 4=Rotten
+	public String name; //Name
+	public String description; //Description
+	public float cycle; //How often does it need to be watered
+	public int waterStatus; //Current water level (0-100)
+	public bool isDead; //True if the plant is dead
+	public int wateringImpact; //Amount of water added per click
+	public int decayRate; //Decay per Time (fromm 100)
 
 
 	// Called when the node enters the scene tree for the first time.
+	public Plant(int cost, int sellValue, int yield, int growDurationInDays, int currentGrowProgress, int stage, string name, string description, float cycle, int wateringImpact, int decayRate)
+	{
+		this.cost = cost;
+		this.sellValue = sellValue;
+		this.yield = yield;
+		this.growDurationInDays = growDurationInDays;
+		this.currentGrowProgress = currentGrowProgress;
+		this.stage = stage;
+		this.name = name;
+		this.description = description;
+		this.cycle = cycle;
+		this.wateringImpact = wateringImpact;
+		this.decayRate = decayRate;
+	}
+
 	public override void _Ready()
 	{
 		CheckForGrowth();
@@ -29,16 +45,18 @@ public partial class Plant : Node
 
 	}
 
-	public float GetWaterStatus(){
+	public double GetWaterStatus(){
 		return waterStatus;
 	}
 
+	public void Decay()
+	{
+		waterStatus = waterStatus - decayRate;
+	}
+	public void SetWaterStatus(float value){}
 	public void WaterPlant(){
 		waterStatus += wateringImpact;
-		if (GetWaterStatus() >= 1) {
-			GD.Print("KYS! Dont kill the plant idio!");
-			this.isDead = true;
-		}
+		
 	}
 
 	public void NextStage(){
@@ -50,6 +68,16 @@ public partial class Plant : Node
 			NextStage();
 		}
 	}
+	
+	public bool CheckForDead(){
+		if (GetWaterStatus() >= 100 || GetWaterStatus() <= 0) {
+			GD.Print("KYS! Dont kill the plant idio!");
+			this.isDead = true;
+			
+		}
+		return isDead;
+	}
+	
 	
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
