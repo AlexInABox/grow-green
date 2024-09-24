b8eaaa1271da4a0c0eb6bef4ca4a28363ba197a1
AlexInABox <main@alexinabox.de>
2024-09-24 14:51:51 +0200

    upstream

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,44 +1,39 @@
 using Godot;
 using System;
-using System.Collections.Generic;
 
 public partial class Player : Node
 {
-    public string username;
-	public List<Plant> listOfOwnedPlants;
-	public int coins;
-	public int characterId;
+    public string name;
+    public int coins;
+    public int character;
 
-	
-    public Player(){ //Development
-		this.username = "Söder";
-		this.listOfOwnedPlants = new List<Plant>();
-		this.listOfOwnedPlants.Add(new Plant());
-		this.coins = 100;
-		this.characterId = 1;
+    public Player(string playerName, int money, int startCharacter)
+	{
+	    name = playerName;
+	    coins = money;
+	    character = startCharacter;
 	}
 
-	public Player(int characterId){ //New User Created
-		this.username = "";
-		this.listOfOwnedPlants = new List<Plant>();
-		this.coins = 100;
-		this.characterId = characterId;
-	}
+    public int AddCoins(int coinsToBeAdded)
+    {
+	    coins += coinsToBeAdded;
+	    return coins;
+    }
 
-	public Player(string username, List<Plant> listOfOwnedPlants, int coins, int characterId){ //Load Player
-		this.username = username;
-		this.listOfOwnedPlants = listOfOwnedPlants;
-		this.coins = coins;
-		this.characterId = characterId;
-	}
-
+    public Player GetCurrentPlayer()
+    {
+	    return this;
+    }
+    
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		AddCoins(1);
 	}
 }
