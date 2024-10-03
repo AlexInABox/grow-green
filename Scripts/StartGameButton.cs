ad345ed1f2a3629bf103fe8f5fb0b46d75fe885e
Alexander Betke <main@alexinabox.de>
2024-10-03 13:42:17 +0200

    remove warning

--- Scripts/StartGameButton.cs
+++ Scripts/StartGameButton.cs
@@ -8,10 +8,10 @@ public partial class StartGameButton : Button
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Pressed += ButtonPressed;
+		Pressed += ButtonPressedEvent;
 	}
 
-	private void ButtonPressed()
+	private void ButtonPressedEvent()
 	{
 		var newScenePath = "res://Scenes/loadPlayerObject.tscn";
 		GetTree().ChangeSceneToFile(newScenePath);
