5dccad872486187e9fc05e38ad456ce472763c94
Maja <majaguentherr@gmail.com>
2024-10-16 12:58:16 +0200

    Minigame Memory und Memory Leave Buttons

---
+++ Scripts/Memory/LeaveButton.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+namespace Memory;
+public partial class LeaveButton : Button
+{
+	SceneManager sceneManager;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		sceneManager = GetNode<SceneManager>("../SceneManager");
+		Pressed += ButtonPressed;
+	}
+
+	private void ButtonPressed()
+	{
+		sceneManager.UpdateSaveBlocking();
+		var mainScene = "res://Scenes/MinigameSzene.tscn";
+		GetTree().ChangeSceneToFile(mainScene);
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
