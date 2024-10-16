bdef128d5dfa27d439f05265b15a9f6efad92816
lagopodus <monsterauge01@gmail.com>
2024-10-16 17:04:09 +0200

    Bigger Bubbles

--- Scripts/PlantWrapper.cs
+++ Scripts/PlantWrapper.cs
@@ -15,7 +15,13 @@ public partial class PlantWrapper : Node2D
 		AddChild(potObject);
 		
 		Label plantName = GetNode<Label>("statusBubble/plantName");
+		Label className = GetNode<Label>("statusBubble/className");
 		plantName.Text = plant.className;
+		className.Text = plant.plantName;
+		Label plantNameL = GetNode<Label>("statusBubbleLeft/plantName");
+		Label classNameL = GetNode<Label>("statusBubbleLeft/className");
+		plantNameL.Text = plant.className;
+		classNameL.Text = plant.plantName;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
