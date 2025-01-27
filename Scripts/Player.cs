4aad48a8a039b66c18b4f3e9719021b91ca85d1e
Alexander Betke <main@alexinabox.de>
2025-01-27 11:42:04 +0100

    added the flappyPlant highscore to the database

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -1,50 +1,60 @@
-using Godot;
-using System;
 using System.Collections.Generic;
+using Godot;
 
 public partial class Player : Node
 {
-	public string username;
-	public List<Plant> listOfOwnedPlants;
-	public List<Pot> listOfOwnedPots;
-	public int coins;
-	public int characterId;
-	public bool greenhouseUnlocked;
-    
-    public Player(){ //Development
-        this.username = "Söder";
-        this.listOfOwnedPlants = new List<Plant>();
-        this.listOfOwnedPots = new List<Pot>();
-        this.coins = 25;
-        this.characterId = 1;
-		this.greenhouseUnlocked = true;
+    public int characterId;
+    public int coins;
+    public int flappyPlantHighscore;
+    public bool greenhouseUnlocked;
+    public List<Plant> listOfOwnedPlants;
+    public List<Pot> listOfOwnedPots;
+    public string username;
+
+    public Player()
+    {
+        //Development
+        username = "Söder";
+        listOfOwnedPlants = new List<Plant>();
+        listOfOwnedPots = new List<Pot>();
+        coins = 25;
+        characterId = 1;
+        greenhouseUnlocked = true;
+        flappyPlantHighscore = 0;
     }
 
-    public Player(int characterId){ //New User Created
-        this.username = "";
-        this.listOfOwnedPlants = new List<Plant>();
-        this.listOfOwnedPots = new List<Pot>();
-        this.coins = 25;
+    public Player(int characterId)
+    {
+        //New User Created
+        username = "";
+        listOfOwnedPlants = new List<Plant>();
+        listOfOwnedPots = new List<Pot>();
+        coins = 25;
         this.characterId = characterId;
-		this.greenhouseUnlocked = false;
+        greenhouseUnlocked = false;
+        flappyPlantHighscore = 0;
     }
 
-	public Player(string username, List<Plant> listOfOwnedPlants, List<Pot> listOfOwnedPots, int coins, int characterId, bool greenhouseUnlocked){ //Load Player
-		this.username = username;
-		this.listOfOwnedPlants = listOfOwnedPlants;
-		this.listOfOwnedPots = listOfOwnedPots;
-		this.coins = coins;
-		this.characterId = characterId;
-		this.greenhouseUnlocked = greenhouseUnlocked;
-	}
+    public Player(string username, List<Plant> listOfOwnedPlants, List<Pot> listOfOwnedPots, int coins, int characterId,
+        bool greenhouseUnlocked, int flappyPlantHighscore)
+    {
+        //Load Player
+        this.username = username;
+        this.listOfOwnedPlants = listOfOwnedPlants;
+        this.listOfOwnedPots = listOfOwnedPots;
+        this.coins = coins;
+        this.characterId = characterId;
+        this.greenhouseUnlocked = greenhouseUnlocked;
+        this.flappyPlantHighscore = flappyPlantHighscore;
+    }
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 }
